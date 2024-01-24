af49c0039adf35e764815950afa8c7f908055a27
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-01-24 09:23:23 -0300

    testes concluidos

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+// o teste : http://localhost:3000/createproduct
 const express = require('express')
 const app = express()
 
-
 app.use(
-    express.urlencoded({
-        extended: true
-    }) 
+  express.urlencoded({
+    extended: true,
+  }), //é responsável por analisar dados provenientes de formulários HTML, e por isso é comumente utilizado em aplicativos web para processar dados de formulários.
 )
 
+app.use(express.json()) // Configurando o Express para analisar automaticamente o corpo das solicitações HTTP como JSON e tornar os dados JSON disponíveis no objeto req.body para que você possa manipulá-los facilmente em rotas e controladores.
 
-app.use(express.json())
+app.post('/createproduct', (req, res) => {
+  const name = req.body.name
+  const price = req.body.price
 
-// rotas 
-app.get('/' , (req,res) =>{ 
-     res.json({message: 'Primeira rota criada com sucesso'})
- 
+  if (!name) {
+    res.status(422).json({ message: `O campo nome precisa ser enviado!` }) // 422 é a mensagem de erro.
+    return
+  }
+
+  console.log(name)
+  console.log(price)
+
+  res
+    .status(201) // 201 é a mensagem positiva 
+    .json({ message: `O produto: ${name} foi criado com sucesso!` })
 })
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Primeira rota criada com sucesso!' })
+})
+
 app.listen(3000)
